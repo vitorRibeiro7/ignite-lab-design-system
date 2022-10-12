bbe24279c2b41eac8da56e49184bbfc82ae4af4a
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-10-11 22:40:40 -0300

    :rocket: Setting new font sizes to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,14 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
     colors: {
       'gray-900': '#121214',
       'gray-800': '#202024',
