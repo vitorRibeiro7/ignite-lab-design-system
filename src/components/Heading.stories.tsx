30b2726b682cd5abee1e5065c51f54e9893afeed
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-10-11 23:43:33 -0300

    :rocket: Heading component added.

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from "./Text";
+import { Heading, HeadingProps } from "./Heading";
 
 export default {
-    title: 'Components/Text',
-    component: Text,
+    title: 'Components/Heading',
+    component: Heading,
     args: {
-        children: 'Hello World',
+        children: 'Lorem Ipsun.',
         size: 'md',
     },
     argTypes: {
@@ -16,29 +16,34 @@ export default {
             }
         }
     }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
     args: {
         size: 'sm'
     }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
     args: {
         size: 'lg'
     }
 }
 
-export const Custom: StoryObj<TextProps> = {
+export const Custom: StoryObj<HeadingProps> = {
     args: {
         asChild: true,
-        children: (<p>Text with P type.</p>)
+        children: (<p>Heading with H1.</p>)
     },
     argTypes: {
         children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
             table: {
                 disable: true,
             }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,24 +2,24 @@ import { ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface TextProps {
+export interface HeadingProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Heading({ size = 'md', children, asChild }: HeadingProps) {
 
-    const Comp = asChild ? Slot : 'span';
+    const Comp = asChild ? Slot : 'h2';
 
     return (
         <Comp
             className={clsx(
-                'text-gray-100 font-sans',
+                'text-gray-100 font-bold font-sans',
                 {
-                    'text-xs': size === 'sm',
-                    'text-sm': size === 'md',
-                    'text-md': size === 'lg',
+                    'text-lg': size === 'sm',
+                    'text-xl': size === 'md',
+                    'text-2xl': size === 'lg',
                 }
             )}
         >
