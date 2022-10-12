fb98b6d0c80f14ae508d7dcdb4cf548a1c3b241a
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-10-11 22:47:13 -0300

    :rocekt: Creating components on storybook.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './styles/global.css'
 
 export function App() {
   return (
-    <h1 className='font-bold text-2xl text-cyan-500'>Hello World</h1>
+    <h1 className='font-bold text-sm text-cyan-500'>Hello World</h1>
   )
 }
---
+++ src/components/text.stories.tsx
@@ -0,0 +1,8 @@
+import { text } from "./text";
+
+export default {
+    title: 'Components/Text',
+    component: text,
+}
+
+export const Default = {};
