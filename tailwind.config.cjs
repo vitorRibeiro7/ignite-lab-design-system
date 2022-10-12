633a343324f56303866fdaf92a9aef53b0405d8d
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-10-11 22:38:48 -0300

    :rocket: Setting new colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx',
+  ],
   theme: {
-    extend: {},
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb',
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
